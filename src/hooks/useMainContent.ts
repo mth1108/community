a4f68045b8a59802a293522b28087770ed6fab64
MunTaeHyeon <teahyeonm@nexmore.co.kr>
2026-08-28 12:17:04 +0900

    배포 테스트

--- src/hooks/useMainContent.ts
+++ src/hooks/useMainContent.ts
@@ -44,7 +44,7 @@ export function useSelectId() {
 
   useEffect(() => {
     axios({
-      url: 'https://glabrescent-squirtingly-diedre.ngrok-free.dev/selectPosts',
+      url: 'https://glabrescent-squirtingly-diedre.ngrok-free.dev/selectPostsId',
       // 나중에 페이지 및 사이즈는 인자로 받을 예정(아마)
       // url: 'http://localhost:8080/selectPostsId',
       method: 'get',
